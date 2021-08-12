f6fe5ff49b16d8f83a3d0d0eae8d579b96f90faa
Robrecht Scheepers <robrecht.scheepers@gmail.com>
2021-08-12 10:37:43 +0200

    Initial Commit

---
+++ testgit/testgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testgitApp.swift
+//  testgit
+//
+//  Created by user199151 on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct testgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
